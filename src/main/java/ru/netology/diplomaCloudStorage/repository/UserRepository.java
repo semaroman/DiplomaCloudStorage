d8d3a47bbb7fcf0958e76e3b0a4695b68384b964
semaroman <106012219+semaroman@users.noreply.github.com>
2024-11-10 16:08:15 +0300

    Update UserRepository.java

--- src/main/java/ru/netology/diplomaCloudStorage/repository/UserRepository.java
+++ src/main/java/ru/netology/diplomaCloudStorage/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package ru.netology.diplomaCloudStorage.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.netology.diplomaCloudStorage.entity.UserEntity;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserByLogin(String login);
 }
