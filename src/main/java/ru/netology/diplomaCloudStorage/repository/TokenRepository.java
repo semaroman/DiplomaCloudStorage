5ade2b112621b3d2b5cf008c93830d62b5e5482d
semaroman <106012219+semaroman@users.noreply.github.com>
2024-11-10 16:07:43 +0300

    Update TokenRepository.java

--- src/main/java/ru/netology/diplomaCloudStorage/repository/TokenRepository.java
+++ src/main/java/ru/netology/diplomaCloudStorage/repository/TokenRepository.java
@@ -1,9 +1,28 @@
 package ru.netology.diplomaCloudStorage.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.netology.diplomaCloudStorage.entity.TokenEntity;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
-public interface TokenRepository extends CrudRepository<TokenEntity, String> {
+public class TokenRepository {
+    private final Map<String, String> tokensAndLogins = new ConcurrentHashMap<>();
+
+    public void putTokenAndLogin(String token, String login) {
+        tokensAndLogins.put(token, login);
+    }
+
+    public void removeTokenAndLoginByToken(String token) {
+        tokensAndLogins.remove(token);
+    }
+
+    public Optional<String> getLoginByToken(String token) {
+        return Optional.ofNullable(tokensAndLogins.get(token));
+    }
+
+    public void deleteAll() {
+        this.tokensAndLogins.clear();
+    }
 }
