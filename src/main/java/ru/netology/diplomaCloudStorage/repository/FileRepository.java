11b928c7e9c832ba1ce5e5a332c9968f55f305b0
semaroman <106012219+semaroman@users.noreply.github.com>
2024-11-10 16:06:24 +0300

    Update FileRepository.java

--- src/main/java/ru/netology/diplomaCloudStorage/repository/FileRepository.java
+++ src/main/java/ru/netology/diplomaCloudStorage/repository/FileRepository.java
@@ -1,13 +1,24 @@
 package ru.netology.diplomaCloudStorage.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import ru.netology.diplomaCloudStorage.entity.FileEntity;
 
 import java.util.List;
 
-@org.springframework.stereotype.Repository
-public interface FileRepository extends CrudRepository<FileEntity, String> {
-    @Query(value = "SELECT * FROM files LIMIT :limit", nativeQuery = true)
-    List<FileEntity> getFiles(int limit);
+public interface FileRepository extends JpaRepository<FileEntity, Long> {
+    @Query(value = "select * from files f where f.user_id = ?1 order by f.id asc limit ?2",
+            nativeQuery = true)
+    List<FileEntity> findFilesByUserIdWithLimit(Long userId, int limit);
+
+    @Query(value = "select id from files f where f.user_id = ?1 and f.name = ?2",
+            nativeQuery = true)
+    List<Long> findFilesByUserIdAndName(Long userId, String name);
+
+    @Query(value = "select * from files f where f.id = ?1 limit 1",
+            nativeQuery = true)
+    FileEntity findFilesById(Long fileId);
+
+    @Query(value = "select * from files", nativeQuery = true)
+    List<FileEntity> findAllFiles();
 }
