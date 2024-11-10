4329f7d6033663899346ebfd1cc50dd4a2ef434c
semaroman <106012219+semaroman@users.noreply.github.com>
2024-11-10 16:11:24 +0300

    Update AuthorizationServiceTest.java

--- src/test/java/ru/netology/diplomaCloudStorage/service/AuthorizationServiceTest.java
+++ src/test/java/ru/netology/diplomaCloudStorage/service/AuthorizationServiceTest.java
@@ -3,7 +3,6 @@ package ru.netology.diplomaCloudStorage.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import ru.netology.diplomaCloudStorage.dto.LoginRequest;
 import ru.netology.diplomaCloudStorage.entity.UserEntity;
 import ru.netology.diplomaCloudStorage.repository.TokenRepository;
 import ru.netology.diplomaCloudStorage.repository.UserRepository;
@@ -12,63 +11,83 @@ import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
-class AuthorizationServiceTest {
+public class AuthorizationServiceTest {
     public static final String TOKEN = "semaroman";
     public static final String UNKNOWN_TOKEN = "alexivanov";
-    public static final String USER = "semashkevich";
-    public static final String UNKNOWN_USER = "ivanov";
-    public static final String CORRECT_PASSWORD = "qwerty123";
+    public static final Long ID = 1L;
+    public static final String LOGIN = "semashkevich";
+    public static final String UNKNOWN_LOGIN = "ivanov";
+    public static final String PASSWORD = "qwerty12345";
+    public static final String INCORRECT_PASSWORD = "password9876";
 
     private final UserRepository userRepository = createUserRepositoryMock();
     private final TokenRepository tokenRepository = createTokenRepositoryMock();
 
     private UserRepository createUserRepositoryMock() {
         final UserRepository userRepository = Mockito.mock(UserRepository.class);
-        when(userRepository.findById(USER)).thenReturn(Optional.of(new UserEntity(USER, CORRECT_PASSWORD)));
-        when(userRepository.findById(UNKNOWN_USER)).thenReturn(Optional.empty());
+
+        when(userRepository.findUserByLogin(LOGIN)).
+                thenReturn(Optional.of(new UserEntity(1L, LOGIN, PASSWORD)));
+        when(userRepository.findUserByLogin(UNKNOWN_LOGIN))
+                .thenReturn(Optional.empty());
         return userRepository;
     }
 
     private TokenRepository createTokenRepositoryMock() {
         final TokenRepository tokenRepository = Mockito.mock(TokenRepository.class);
-        when(tokenRepository.existsById(TOKEN.split(" ")[1].trim())).thenReturn(true);
-        when(tokenRepository.existsById(UNKNOWN_TOKEN)).thenReturn(false);
+
+        when(tokenRepository.getLoginByToken(TOKEN.split(" ")[1].trim()))
+                .thenReturn(Optional.of(LOGIN));
+        when(tokenRepository.getLoginByToken(UNKNOWN_TOKEN))
+                .thenReturn(Optional.empty());
         return tokenRepository;
     }
 
     @Test
-    void login() {
-        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository);
-        Assertions.assertDoesNotThrow(() -> authorizationService.login(new LoginRequest(USER, CORRECT_PASSWORD)));
+    void testLogin() {
+        final AuthorizationService authorizationService =
+                new AuthorizationService(userRepository, tokenRepository);
+        Assertions.assertDoesNotThrow(() -> authorizationService
+                .login(new UserEntity(ID, LOGIN, PASSWORD)));
     }
 
     @Test
-    void login_userNotFound() {
-        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository);
-        Assertions.assertThrows(RuntimeException.class, () -> authorizationService.login(new LoginRequest(UNKNOWN_USER, CORRECT_PASSWORD)));
+    void testUnknownLogin() {
+        final AuthorizationService authorizationService =
+                new AuthorizationService(userRepository, tokenRepository);
+        Assertions.assertThrows(RuntimeException.class, () -> authorizationService
+                .login(new UserEntity(ID, UNKNOWN_LOGIN, PASSWORD)));
     }
 
     @Test
-    void login_incorrectPassword() {
-        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository);
-        Assertions.assertThrows(RuntimeException.class, () -> authorizationService.login(new LoginRequest(USER, "abcdef")));
+    void testIncorrectPassword() {
+        final AuthorizationService authorizationService =
+                new AuthorizationService(userRepository, tokenRepository);
+        Assertions.assertThrows(RuntimeException.class, () -> authorizationService
+                .login(new UserEntity(ID, LOGIN, INCORRECT_PASSWORD)));
     }
 
     @Test
-    void logout() {
-        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository);
-        Assertions.assertDoesNotThrow(() -> authorizationService.logout(TOKEN));
+    void testLogout() {
+        final AuthorizationService authenticationService =
+                new AuthorizationService(userRepository, tokenRepository);
+        Assertions.assertDoesNotThrow(() -> authenticationService
+                .logout(TOKEN));
     }
 
     @Test
-    void checkToken() {
-        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository);
-        Assertions.assertDoesNotThrow(() -> authorizationService.checkToken(TOKEN));
+    void testToken() {
+        final AuthorizationService authorizationService =
+                new AuthorizationService(userRepository, tokenRepository);
+        Assertions.assertDoesNotThrow(() -> authorizationService
+                .checkToken(TOKEN));
     }
 
     @Test
-    void checkToken_failed() {
-        final AuthorizationService authorizationService = new AuthorizationService(userRepository, tokenRepository);
-        Assertions.assertThrows(RuntimeException.class, () -> authorizationService.checkToken(UNKNOWN_TOKEN));
+    void testUnknownToken() {
+        final AuthorizationService authorizationService =
+                new AuthorizationService(userRepository, tokenRepository);
+        Assertions.assertThrows(RuntimeException.class, () -> authorizationService
+                .checkToken(UNKNOWN_TOKEN));
     }
 }
